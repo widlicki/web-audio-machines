3609b2aac952ce4589617e3be0b272ae26a23eae
igor <igor.widlicki@ardanis.com>
2022-02-23 23:37:55 +0000

    add cypress testing

--- tests/unit/pages/index.test.tsx
+++ tests/unit/pages/index.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-import Home from '../pages/index'
+import Home from '../../../pages/index'
 
 describe('Home', () => {
   it('renders homepage according to snapshot', () => {
--- tests/unit/pages/sign-up.test.tsx
+++ tests/unit/pages/sign-up.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-import SignUp from '../pages/sign-up'
+import SignUp from '../../../pages/sign-up'
 
 describe('Sign Up', () => {
   it('renders sign-up page according to snapshot', () => {
