e47199998048baa4d949e5dbf5cf5858fa7ef321
Igor Widlicki <widlicki@gmail.com>
2022-05-31 21:04:13 -0400

    test push

--- components/machines/StepPad/StepPad.tsx
+++ components/machines/StepPad/StepPad.tsx
@@ -22,7 +22,7 @@ const StepPad = ({
           : `${borderSize} dashed ${borderColor}`,
         borderRadius: theme.border.radius.sm,
         minWidth: '60px',
-        minHeight: '50px',
+        minHeight: '55px',
       }}
     />
   )
